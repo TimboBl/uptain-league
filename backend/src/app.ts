a2b8cbfefc9d45f97a314cbd99cbcc2128675a46
Timbo <5blaesche@informatik.uni-hamburg.de>
2017-12-30 14:02:26 +0100

    Added route files with respective router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,20 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
+import {logger} from "./logging/logger";
+import * as scoreKeeping from "./routes/scoreKeeping";
+import * as access from "./routes/access";
 
-const startApp = () => {
+export const startApp = () => {
+    logger.debug("Starting app");
     const app = express();
 
+    logger.debug("Instantiating middlewares");
     app.use(bodyParser.json());
     app.use(cors());
+
+    logger.debug("Registering routes");
+
+    app.use(access.getRouter());
+    app.use("/board", scoreKeeping.getRouter());
 };

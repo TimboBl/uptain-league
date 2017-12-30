47d255f195243f20f8e63f756131bc1fc6a9104c
Timbo <5blaesche@informatik.uni-hamburg.de>
2017-12-30 17:17:51 +0100

    Removed unsupported option in mongo driver

--- backend/src/services/mongoService.ts
+++ backend/src/services/mongoService.ts
@@ -1,20 +1,22 @@
 import * as mongoose from "mongoose";
 import {logger} from "../logging/logger";
 import {Player} from "Player";
+import {PLAYER} from "../models/Player";
 
 export const mongoService = (() => {
 
-    const createPlayer = (name: string): Promise<Player> => {
-        return new Promise(((resolve: Function, reject: Function) => {
+    const updatePlayerScore = (name: string, score: number): Promise<Player> => {
 
-        }));
+           return PLAYER.update({"name": name}, {
+                "$set": {"name": name, "score": score}
+            }, {upsert: true}).exec();
     };
 
-    const mongoMethods = {createPlayer};
+    const mongoMethods = {updatePlayerScore};
     const init = () => {
         return new Promise((resolve: Function, reject: Function) => {
-            mongoose.connect(process.env.MONGO || "mongodb://localhost:27017/uptain-board",
-                {useMongoClient: true}).then(() => {
+            mongoose.connect(process.env.MONGO || "mongodb://localhost:27017/uptain-board")
+                .then(() => {
                 resolve(mongoMethods);
             }).catch((err: Error) => {
                 logger.error("Could not establish a connection to MongoDB", {error: err});

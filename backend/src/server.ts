4892ad0bad1336c19bc1fee2b269b3c90636c975
Timbo <5blaesche@informatik.uni-hamburg.de>
2017-12-30 14:33:25 +0100

    Added catch and process exit, when failing to connect to db or app has crashed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,4 +8,7 @@ mongoService.init().then((mongoDB: any) => {
     return startApp(mongoDB);
 }).then(() => {
     logger.debug("Server is listening at", {port: process.env.PORT || 3001});
+}).catch(() => {
+    logger.error("Starting the server failed... \nExiting");
+    process.exit(-1);
 });

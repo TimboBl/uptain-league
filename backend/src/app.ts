f9f8b9cd5901a56bc28283764077d2968dfbc94c
CkFreak <5blaesche@informatik.uni-hamburg.de>
2017-12-30 13:31:08 +0100

    Started to setup the web server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,10 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+
+const startApp = () => {
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(cors());
+};

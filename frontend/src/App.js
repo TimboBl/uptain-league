f126f8cb0b6dd76f87e27fe90fe65892039a56ae
Timbo <5blaesche@informatik.uni-hamburg.de>
2017-12-31 14:59:57 +0100

    Created a header with the uptain logo and headline, started to build leader table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header /*style={{backgroundColor: "rgb(147, 212, 217"}}*/>
+                    <a href={"http://www.uptain.de"} ><img src={"http://www.uptain.de/wp-content/uploads/2016/06/logo-1.png"} alt={"This should be a logo"}
+                                     height={40} width={108} style={{float: "left", paddingLeft: "40px", paddingTop: "10px"}}/></a>
+                    <h1 className={"headline"} style={{paddingTop: "50px", textAlign: "center"}}>uptain Leaderbord</h1>
+                </header>
+                <body>
+                <table style={{margin: "auto"}}>
+                    <th>
+                        <td className={"tableHeader"} style={{paddingRight: "10px"}}>Player</td>
+                        <td className={"tableHeader"} style={{paddingLeft: "10px"}}>Score</td>
+                    </th>
+                </table>
+                </body>
+            </div>
+        );
+    }
 }
 
 export default App;

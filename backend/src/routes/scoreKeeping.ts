3f652f21172c059f72a07faf74ae813aa8f9252b
Timbo <5blaesche@informatik.uni-hamburg.de>
2017-12-31 16:37:34 +0100

    Added method to /scores route

--- backend/src/routes/scoreKeeping.ts
+++ backend/src/routes/scoreKeeping.ts
@@ -5,7 +5,7 @@ export const getRouter = (mongoDB: any) => {
     const router = express.Router();
     const playerController = PlayerController.playerController(mongoDB);
 
-    router.get("/scores");
+    router.get("/scores", playerController.getScores);
     router.put("/scores/:player", playerController.updateScore);
 
     return router;

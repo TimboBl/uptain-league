1e5bc3c91b532df71cf385f6f71fed9109e1ab96
Timbo <5blaesche@informatik.uni-hamburg.de>
2017-12-31 15:42:46 +0100

    Added background and text colors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,25 +13,28 @@ class App extends Component {
         return (
             <div className="App">
                 <header /*style={{backgroundColor: "rgb(147, 212, 217"}}*/>
-                    <a href={"http://www.uptain.de"} ><img src={"http://www.uptain.de/wp-content/uploads/2016/06/logo-1.png"} alt={"uptain.de"}
-                                     height={40} width={108} style={{float: "left", paddingLeft: "40px", paddingTop: "10px"}}/></a>
+                    <a href={"http://www.uptain.de"}><img
+                        src={"http://www.uptain.de/wp-content/uploads/2016/06/logo-1.png"} alt={"uptain.de"}
+                        height={40} width={108} style={{float: "left", paddingLeft: "40px", paddingTop: "10px"}}/></a>
                     <h1 className={"headline"} style={{paddingTop: "50px", textAlign: "center"}}>uptain Leaderbord</h1>
                 </header>
-                <table style={{margin: "auto"}}>
-                    <thead>
-                    <tr>
-                        <th className={"tableHeader"} style={{paddingRight: "10px"}}>Player</th>
-                        <th className={"tableHeader"} style={{paddingLeft: "10px"}}>Score</th>
-                    </tr>
-                    </thead>
-                    <tbody>{this.getPlayers()}</tbody>
-                </table>
+                <div style={{backgroundColor: "#5b5553", width: "100%"}}>
+                    <table style={{margin: "auto"}}>
+                        <thead>
+                        <tr>
+                            <th className={"tableHeader"} style={{paddingRight: "10px"}}>Player</th>
+                            <th className={"tableHeader"} style={{paddingLeft: "10px"}}>Score</th>
+                        </tr>
+                        </thead>
+                        <tbody>{this.getPlayers()}</tbody>
+                    </table>
+                </div>
             </div>
         );
     }
 
     getPlayers() {
-        return <CustomTableRow key={1} keyID={1} player={"Timbo"} score={"10"}/>
+        return <CustomTableRow key={1} keyID={1} player={"Test"} score={"10"}/>
     }
 }
 

c171d726fdbacb5f5070dea7236e0125ac8abde2
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-01-14 16:55:39 +0100

    Found the array one level deeper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,12 +56,13 @@ class App extends Component {
 
     getPlayers() {
         let rows = [];
+        let self = this;
         axios.get(BASE_URL + SCORES).then((result) => {
            for (let i = 0; i < result.data.length; ++i) {
-               rows.push(<CustomTableRow key={i} keyID={i} player={result.data[i].name}
-                                                                  score={result.data[i].score}/>);
+               rows.push(<CustomTableRow key={i} keyID={i} player={result.data.data[i].name}
+                                                                  score={result.data.data[i].score}/>);
            }
-           this.setState({rows: rows});
+           self.setState({rows: rows});
         }).catch((error) => {
             console.log(error);
         });

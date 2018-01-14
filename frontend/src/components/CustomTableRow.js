66afa91e6f6680926f56ee6340b843dd2e842c6b
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-01-14 16:00:54 +0100

    Added buttons to increase and decrease a players score

--- frontend/src/components/CustomTableRow.js
+++ frontend/src/components/CustomTableRow.js
@@ -13,6 +13,8 @@ class CustomTableRow extends React.Component {
                 <tr className={"tableRow"}>
                     <td>{this.props.player}</td>
                     <td>{this.props.score}</td>
+                    <td><button>Increase Score</button></td>
+                    <td><button>Decrease Score</button></td>
                 </tr>
         )
     }

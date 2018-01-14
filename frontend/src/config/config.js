c3d55a01065611dea810eb82e7ad491ad9f1c5cb
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-01-14 20:04:17 +0100

    Added new routes

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -3,3 +3,5 @@ export const BASE_URL = "http://localhost:3001"; //"timbo.link/uptain-league";
 export const SCORES = "/board/scores";
 
 export const NEW_PLAYER = "/board/player";
+
+export const UPDATE_SCORE = "/board/scores/";

42b86362e5d463f65e965f1249d05e512c7515f8
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-01-23 13:24:35 +0100

    Added more fields to player to enable Elo System

--- backend/src/models/Player.ts
+++ backend/src/models/Player.ts
@@ -2,7 +2,10 @@ import {model, Schema} from "mongoose";
 
 const playerSchema = new Schema({
     name: {type: String},
-    score: {type: Number}
+    score: {type: Number},
+    totalGames: {type: Number},
+    wins: {type: Number},
+    losses: {type: Number}
 });
 
 export const PLAYER = model("Player", playerSchema);

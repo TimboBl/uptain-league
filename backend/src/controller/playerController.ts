6befaf9432cadef2f37ffcdfcd958e4c105882d2
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-01-23 18:29:54 +0100

    Fixed Elo calculation

--- backend/src/controller/playerController.ts
+++ backend/src/controller/playerController.ts
@@ -3,10 +3,12 @@ import {logger} from "../logging/logger";
 import {Player} from "Player";
 
 export const playerController = (mongoDB: any) => {
+    const K = 32;
+
     const updateScore = (req: Request, res: Response) => {
         let oldPlayer: Player;
         let opponentPlayer: Player;
-        let newPlayers: {player: Player, opponent: Player};
+        let newPlayers: { player: Player, opponent: Player };
         mongoDB.findPlayer(req.body.name).then((player: Player) => {
             oldPlayer = player;
             return mongoDB.findPlayer(req.body.opponent);
@@ -28,23 +30,23 @@ export const playerController = (mongoDB: any) => {
     const getScores = (req: Request, res: Response) => {
         logger.debug("Getting scores");
         const cursor = mongoDB.getScores();
-            const player: Player[] = [];
-            cursor.on("data", (doc: Player) => {
-                player.push(doc);
-            });
-            cursor.on("error", (err: Error) => {
-                logger.error("Cursor for getting Scores ran into an error", {error: err});
-                res.status(500).send({message: "There was an internal server error"});
-                return;
-            });
-            cursor.on("end", () => {
-                logger.debug("Getting the scores was successful");
-                res.status(200).send({message: "Success", data: player});
-            });
+        const player: Player[] = [];
+        cursor.on("data", (doc: Player) => {
+            player.push(doc);
+        });
+        cursor.on("error", (err: Error) => {
+            logger.error("Cursor for getting Scores ran into an error", {error: err});
+            res.status(500).send({message: "There was an internal server error"});
+            return;
+        });
+        cursor.on("end", () => {
+            logger.debug("Getting the scores was successful");
+            res.status(200).send({message: "Success", data: player});
+        });
     };
 
     const saveNewPlayer = (req: Request, res: Response) => {
-        if(req.body.name) {
+        if (req.body.name) {
             mongoDB.findPlayer(req.body.name).then((result: any) => {
                 if (result) {
                     res.status(409).send({message: "Conflict! This name is already taken"});
@@ -62,22 +64,27 @@ export const playerController = (mongoDB: any) => {
         }
     };
 
-    const updatePlayerScore = (pl: Player, opp: Player, result: number): {player: Player, opponent: Player} => {
+    const updatePlayerScore = (pl: Player, opp: Player, result: number): { player: Player, opponent: Player } => {
         const returnValue = {
             player: pl,
             opponent: opp
         };
 
+        const r1 = Math.pow(10, (pl.score / 400));
+        const r2 = Math.pow(10, (opp.score / 400));
+        const e1 = r1 / (r1 + r2);
+        const e2 = r2 / (r1 + r2);
+        const s1 = result === 0 ? 0 : 1;
+        const s2 = result === 1 ? 1 : 0;
+        pl.score = pl.score + K * (s1 - e1);
+        opp.score = opp.score + K * (s2 - e2);
+
         if (result === 0/*The player has lost to its opponent*/) {
             pl.losses += 1;
             opp.wins += 1;
-            pl.score = opp.score - 400 * (pl.wins - pl.losses);
-            opp.score = pl.score + 400 * (opp.wins - opp.losses);
         } else {
             pl.wins += 1;
             opp.losses += 1;
-            pl.score = opp.score - 400 * (pl.wins - pl.losses);
-            opp.score = pl.score + 400 * (opp.wins - opp.losses);
         }
         pl.totalGames += 1;
         opp.totalGames += 1;

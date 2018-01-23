f8145100006bbde6085d13e3a54e4fcb414ef21c
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-01-23 15:56:25 +0100

    Adjusted Type of Player for new Elo system

--- backend/src/types/Player.d.ts
+++ backend/src/types/Player.d.ts
@@ -2,5 +2,8 @@ declare module "Player" {
     interface Player {
         name: string;
         score: number;
+        wins: number;
+        losses: number;
+        totalGames: number;
     }
 }

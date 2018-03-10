45eb9cfab1e53729958f25aaa8e8e5648a7cc066
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-03-10 12:26:23 +0100

    Adds a match array to the player object

--- backend/src/models/Player.ts
+++ backend/src/models/Player.ts
@@ -1,12 +1,20 @@
 import {model, Schema} from "mongoose";
 
+
+const matchSchema = new Schema({
+    winner: {type: String},
+    looser: {type: String},
+    result: {type: String},
+}, {_id: false});
+
+
 const playerSchema = new Schema({
     name: {type: String},
     score: {type: Number},
     totalGames: {type: Number},
     wins: {type: Number},
     losses: {type: Number},
-    matches: {type: Array}
+    matches: [matchSchema]
 });
 
 export const PLAYER = model("Player", playerSchema);
--- backend/src/types/Player.d.ts
+++ backend/src/types/Player.d.ts
@@ -1,9 +1,17 @@
-declare module "Player" {
-    interface Player {
-        name: string;
-        score: number;
-        wins: number;
-        losses: number;
-        totalGames: number;
-    }
+import {Types} from "mongoose";
+
+interface Player {
+    _id: Types.ObjectId;
+    name: string;
+    score: number;
+    wins: number;
+    losses: number;
+    totalGames: number;
+    matches: [
+        {
+            winner: string,
+            looser: string,
+            result: string,
+        }
+        ];
 }

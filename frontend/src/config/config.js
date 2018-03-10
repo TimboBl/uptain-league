4f20a693a36e830a183a80387b57acea1085f6a3
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-03-10 12:29:09 +0100

    Removes the unused url for base url

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://ec2-35-158-216-96.eu-central-1.compute.amazonaws.com:5005"; //"timbo.link/uptain-league";
+export const BASE_URL = "http://ec2-35-158-216-96.eu-central-1.compute.amazonaws.com:5005";
 
 export const SCORES = "/board/scores";
 
